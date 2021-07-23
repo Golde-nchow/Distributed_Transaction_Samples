7311bbdfe4575071c116508ab53cef5c8fcf0779
曹金洲 <570384231@qq.com>
2021-07-23 21:50:00 +0800

    用户、商品、订单模块，添加简单的业务代码

--- product-biz/product-entity/src/main/java/product/entity/Product.java
+++ product-biz/product-entity/src/main/java/product/entity/Product.java
@@ -1,9 +1,15 @@
 package product.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @TableName("c_product")
 public class Product {
 
